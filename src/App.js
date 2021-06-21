5b47482505a58b10b287dfa7af292836acd2670c
Acekiller433 <acegaskin@gmail.com>
2021-06-21 14:40:37 -0400

    Restructured for ease of viewing and added TODOs

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App()
 }
 
 
+/****************************** InputComponents ****************************************/
 // Create input components for the user to use
 class InputComponents extends React.Component
 {
@@ -84,6 +85,7 @@ class InputComponents extends React.Component
 }
 
 
+/********************************** PerformAPICall ****************************************/
 // Perform Google Maps Place Search API call when "CHANCE!!!" button is pressed
 class PerformAPICall extends React.Component
 {
@@ -95,6 +97,7 @@ class PerformAPICall extends React.Component
       }
    }
 
+
    //// TODO: perform error check on user input before allowing API call
    errorCheckUserInput()
    {
@@ -104,12 +107,13 @@ class PerformAPICall extends React.Component
       this.runAPISearchhUsingUserInput();
    }
 
+
    async runAPISearchhUsingUserInput()
    {
       let userLatitude = 0;
       let userLongitude = 0;
       
-      await CallGetUserPosition().then((position) => {
+      await this.callGetUserPosition().then((position) => {
          userLatitude = position.coords.latitude;
          userLongitude = position.coords.longitude;
       })
@@ -130,116 +134,119 @@ class PerformAPICall extends React.Component
 
       console.log('URL = ' + placeSearchURL);
 
-      await CheckIfResponseIsMoreThanOnePage(placeSearchURL)
+      await this.checkIfResponseIsMoreThanOnePage(placeSearchURL)
       .then(places => { this.setState({places: places}); })
    }
+
+
+   // Grab the current user location and return a Promise while processing
+   // https://gist.github.com/varmais/74586ec1854fe288d393
+   callGetUserPosition()
+   {
+      let GetUserPosition = new Promise( (resolve, reject) => {
+         navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      return GetUserPosition;
+   }
+
+
+   async checkIfResponseIsMoreThanOnePage(placeSearchURL)
+   {
+      let nextPageToken = '';
+      let returnPlaces = [];
+
+      let tempSearchURL = placeSearchURL;
+
+      // Fetch the 1st page of data
+      await this.fetchPlaceSearchJSON(tempSearchURL, 0).then( data => {
+         returnPlaces = data[0];
+         nextPageToken = data[1];
+      })
+      .then(() => this.delay(2000))   // Give time for Google to create next page
+      .then(async () => {
+
+         // If 2nd page exists then fetch next page
+         if (nextPageToken)
+         {
+            tempSearchURL = placeSearchURL +
+            '&pagetoken=' + nextPageToken;
+
+            await this.fetchPlaceSearchJSON(tempSearchURL).then( data => {
+               returnPlaces = returnPlaces.concat(data[0]);
+               nextPageToken = data[1];
+            })
+            .then(() => this.delay(2000))   // Give time for Google to create next page
+            .then( async () => {
+
+               // If 3rd page exists then fetch next page
+               if (nextPageToken)
+               {
+                  tempSearchURL = placeSearchURL +
+                  '&pagetoken=' + nextPageToken;
+
+                  await this.fetchPlaceSearchJSON(tempSearchURL).then( data => {
+                     returnPlaces = returnPlaces.concat(data[0]);
+                     nextPageToken = data[1];
+                  })
+                  .catch(e => console.log(e));  
+               }
+            })
+            .catch(e => console.log(e));
+         }
+      })
+      .catch(e => console.log(e));
+
+      return returnPlaces;
+   }
+
+
+   async fetchPlaceSearchJSON(placeSearchURL)
+   {
+      // Wait for fetch to finish and throw errors if any
+      let response = await fetch(placeSearchURL);
+      if (!response.ok)
+         throw new Error('HTTP error! Status: ' + response.status);
+
+      // Parse response to get list of different restaurants
+      let responseJson = await response.json();
+
+
+      let places = await responseJson.results;
+
+      let nextPageToken = await responseJson.next_page_token;
+
+      //// TODO: Add a setting that removes any restaurants that are currently closed
+      // resultJson = await resultJson.filter(place => place.opening_hours.open_now === true)
+
+      return [places, nextPageToken];   
+   }
+
+
+   // Delay code by inputted amount and return promise instead of callback
+   // https://stackoverflow.com/questions/33843091/how-do-you-wrap-settimeout-in-a-promise/33843314
+   delay(ms)
+   {
+      return new Promise(resolve => setTimeout( () => resolve(), ms));
+   }
+
 
    render()
    {
       return(
          <div>
             <button onClick={() => this.errorCheckUserInput()}>CHANCE!!!</button>
-            <RandomlyChooseARestaurantAndDisplayData places = {this.state.places} />
+            <RandomlyChooseARestaurantAndDisplayData
+               places = {this.state.places} 
+               openNow = {this.props.userInputData.openNow}
+            />
          </div>
       );
    }
 }
 
 
-// Grab the current user location and return a Promise while processing
-   // In function so it does not grab location w/o user action
-// https://gist.github.com/varmais/74586ec1854fe288d393
-// Removed the options param from example as it was not needed
-function CallGetUserPosition()
-{
-   let GetUserPosition = new Promise( (resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
-   });
-
-   return GetUserPosition;
-}
-
-
-async function CheckIfResponseIsMoreThanOnePage(placeSearchURL)
-{
-   let nextPageToken = '';
-   let returnPlaces = [];
-
-   let tempSearchURL = placeSearchURL;
-
-   // Fetch the 1st page of data
-   await  FetchPlaceSearchJSON(tempSearchURL, 0).then( data => {
-      returnPlaces = data[0];
-      nextPageToken = data[1];
-   })
-   .then(() => Delay(2000))   // Give time for Google to create next page
-   .then(async () => {
-
-      // If 2nd page exists then fetch next page
-      if (nextPageToken)
-      {
-         tempSearchURL = placeSearchURL +
-         '&pagetoken=' + nextPageToken;
-
-         await FetchPlaceSearchJSON(tempSearchURL).then( data => {
-            returnPlaces = returnPlaces.concat(data[0]);
-            nextPageToken = data[1];
-         })
-         .then(() => Delay(2000))   // Give time for Google to create next page
-         .then( async () => {
-
-            // If 3rd page exists then fetch next page
-            if (nextPageToken)
-            {
-               tempSearchURL = placeSearchURL +
-               '&pagetoken=' + nextPageToken;
-
-               await FetchPlaceSearchJSON(tempSearchURL).then( data => {
-                  returnPlaces = returnPlaces.concat(data[0]);
-                  nextPageToken = data[1];
-               })
-               .catch(e => console.log(e));  
-            }
-         })
-         .catch(e => console.log(e));
-      }
-   })
-   .catch(e => console.log(e));
-
-   return returnPlaces;
-}
-
-
-async function FetchPlaceSearchJSON(placeSearchURL)
-{
-   // Wait for fetch to finish and throw errors if any
-   let response = await fetch(placeSearchURL);
-   if (!response.ok)
-      throw new Error('HTTP error! Status: ' + response.status);
-
-   // Parse response to get list of different restaurants
-   let responseJson = await response.json();
-
-
-   let places = await responseJson.results;
-
-   let nextPageToken = await responseJson.next_page_token;
-
-   //// TODO: Add a setting that removes any restaurants that are currently closed
-   // resultJson = await resultJson.filter(place => place.opening_hours.open_now === true)
-
-   return [places, nextPageToken];   
-}
-
-
-// Return a promise for places after timeout has elapsed
-// https://stackoverflow.com/questions/33843091/how-do-you-wrap-settimeout-in-a-promise/33843314
-function Delay(ms)
-{
-   return new Promise(resolve => setTimeout( () => resolve(), ms));
-}
-
-
+/********************* RandomlyChooseARestaurantAndDisplayData ****************************/
 class RandomlyChooseARestaurantAndDisplayData extends React.Component
 {
    constructor(props)
@@ -250,26 +257,63 @@ class RandomlyChooseARestaurantAndDisplayData extends React.Component
       };
    }
 
+
+   removeDuplicateAndClosedRestaurants(places)
+   {
+      let filteredPlaces = places;
+
+      //// TODO: Remove duplicate restaurants starting with farthest from user
+
+
+      //// TODO: Remove closed restaurants
+
+
+      return filteredPlaces;
+   }
+
    componentDidUpdate(prevProps)
    {
       // Randomly choose a place and save it
       if(prevProps.places !== this.props.places)
       {
-         const max = this.props.places.length;
+         const filteredPlaces = this.removeDuplicateAndClosedRestaurants(this.props.places);
+         const max = filteredPlaces.length;
          const rand =  Math.floor(Math.random() * max);
          console.log('Max = ' + max);
          console.log('Random = ' + rand);
-         this.setState({randomPlace: this.props.places[rand]});
+         this.setState({randomPlace: filteredPlaces[rand]});
 
-         console.log(this.props.places[rand]);
+         console.log(filteredPlaces[rand]);
       }
+   }
+
+   
+   displayRestaurantData(randomPlace)
+   {
+      console.log('Place name = ' + randomPlace.name);
+
+      // Display restaurant data ***Note: no price b/c often not available***
+      const restName = <li>{randomPlace.name}</li>;
+      const restPicture = <li>Insert Picture</li>;    //// TODO: add img
+      const restRating = <li>{randomPlace.rating}</li>
+      const restLocation = <li>{randomPlace.vicinity}</li>;
+
+      return (
+         <ul>
+            {restName}
+            {restPicture}
+            {restRating}
+            {restLocation}
+         </ul>
+      )
+
    }
 
    render()
    {
       return(
          <div id = 'placeID'>
-            {this.state.randomPlace.name ? displayRestaurantData(this.state.randomPlace)
+            {this.state.randomPlace.name ? this.displayRestaurantData(this.state.randomPlace)
                : <h1>Empty</h1>
             }
          </div>
@@ -277,26 +321,8 @@ class RandomlyChooseARestaurantAndDisplayData extends React.Component
    }
 }
 
-function displayRestaurantData(randomPlace)
-{
-   console.log('Place name = ' + randomPlace.name);
 
-   // Display restaurant data ***Note: no price b/c often not available***
-   const restName = <li>{randomPlace.name}</li>;
-   const restPicture = <li>Insert Picture</li>;    //// TODO: add img
-   const restRating = <li>{randomPlace.rating}</li>
-   const restLocation = <li>{randomPlace.vicinity}</li>;
 
-   return (
-      <ul>
-         {restName}
-         {restPicture}
-         {restRating}
-         {restLocation}
-      </ul>
-   )
-
-}
 
 
 //// TODO: Move the API key and other sensitive data into secure document
